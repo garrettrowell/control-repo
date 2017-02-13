f5800cb5a35cfddc8819dee9a515fbe748e8bc50
Garrett Rowell <garrett2@cnwr.com>
2017-02-13 17:26:28 -0500

    wire up php + template

--- site/profile/manifests/site_db.pp
+++ site/profile/manifests/site_db.pp
@@ -1,10 +1,15 @@
 define profile::site_db (
-  String $db_ensure  = 'present',
-  String $db_user    = $name,
-  String $db_passwd  = hiera("${name}::db_pass"),
-  String $db_host    = 'localhost',
-  String $db_path    = "/tmp/${name}.sql",
-  Array[String] $db_grant = ['SELECT', 'UPDATE'],
+  String                $db_ensure    = 'present',
+  String                $db_user      = $name,
+  String                $db_passwd    = hiera("${name}::db_pass"),
+  String                $db_host      = 'localhost',
+  String                $db_path      = "/tmp/${name}.sql",
+  Array[String]         $db_grant     = ['SELECT', 'UPDATE'],
+  String                $site_ensure  = 'present',
+  String                $site_docroot = "/var/www/${name}",
+  String                $site_owner   = 'root',
+  String                $site_group   = 'root',
+  Pattern[/([0-7]{4})/] $site_mode    = '0644',
 ){
 
   file { "${db_path}":
@@ -20,5 +25,15 @@ define profile::site_db (
     grant    => $db_grant,
     sql      => $db_path,
     require  => File["${db_path}"],
+  }
+
+  file { "${name}.index":
+    path    => "${site_docroot}/index.php",
+    ensure  => $site_ensure,
+    owner   => $site_owner,
+    group   => $site_group,
+    mode    => $site_mode,
+    content => template('profile/web/site.php.erb'),
+    require => Apache::Vhost["${name}.example.com"],
   }
 }
