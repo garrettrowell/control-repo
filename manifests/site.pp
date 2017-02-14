a5134795500b1a035e0366e8c1153c1beeb4f630
Garrett Rowell <garrett2@cnwr.com>
2017-02-14 13:38:08 -0500

    cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,37 +5,15 @@
 File { backup => false }
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
 }
 
 node 'cent-agent.localdomain' {
     include ::role::apache_sites
-
-    class { '::php':
-      ensure       => latest,
-      manage_repos => true,
-      fpm          => true,
-      dev          => true,
-      composer     => true,
-      pear         => true,
-      phpunit      => false,
-    }
+    include ::role::db_setup
 
     #Only needed when using concat module tag 1.1.0,
     # order needs to be a string. Fixed in tag >= 1.1.1
     Concat::Fragment <| |> {
       order => '10',
-    }
-
-    #needs to be moved into roles + profiles
-    class { '::mysql::server':
-      root_password           => 'strongpassword',
-      remove_default_accounts => true,
-    }
-
-    class { '::mysql::bindings':
-      php_enable => true,
     }
 }
