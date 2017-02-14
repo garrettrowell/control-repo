000e04e54bd446fdfa2eab5bccc6b33c7f3eef58
Garrett Rowell <garrett2@cnwr.com>
2017-02-14 13:53:09 -0500

    remove kludges in prod

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,10 +10,4 @@ node default {
 node 'cent-agent.localdomain' {
     include ::role::apache_sites
     include ::role::db_setup
-
-    #Only needed when using concat module tag 1.1.0,
-    # order needs to be a string. Fixed in tag >= 1.1.1
-    Concat::Fragment <| |> {
-      order => '10',
-    }
 }
